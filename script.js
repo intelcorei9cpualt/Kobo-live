48a6af51c3ce54106c02b1efc08bef2412750766
intelcorei9cpualt <363173709@gotvdsb.ca>
2026-07-12 18:08:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const player = document.getElementById("player");
+
+const upload = document.getElementById("upload");
+
+upload.addEventListener("change", () => {
+
+alert("Upload system coming in Step 2!");
+
+});
