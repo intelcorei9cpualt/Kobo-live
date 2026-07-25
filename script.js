468c48a7343236c89d37e5a42fd04193f7e8fc9e
intelcorei9cpualt <363173709@gotvdsb.ca>
2026-07-25 10:43:28 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const player = document.getElementById("player");
-
-const upload = document.getElementById("upload");
-
-upload.addEventListener("change", () => {
-
-alert("Upload system coming in Step 2!");
-
-});
